f30fa5edc0962631931890482f89d6024a1c949e
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-26 12:10:24 -0300

    fix: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) }, { path: 'not-found', loadChildren: () => import('./components/pages/not-found/not-found/not-found.module').then(m => m.NotFoundModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }, { path: 'character-details', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) }, { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) }];
+const routes: Routes = [
+  { path: '**', redirectTo: 'character-list', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
+  { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
+  { path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) },
+  { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) },
+  { path: 'not-found', loadChildren: () => import('./components/pages/not-found/not-found/not-found.module').then(m => m.NotFoundModule) }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
