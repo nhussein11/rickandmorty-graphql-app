2584aae27a9c767212bd87a521ae6059ca43226b
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-26 11:49:24 -0300

    add: more initial project structure

---
+++ src/app/components/pages/characters/character-card/character-card.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterCardComponent } from './character-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    CharacterCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharacterCardModule { }
