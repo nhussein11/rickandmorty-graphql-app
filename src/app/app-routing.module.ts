5bf87eb254b868ed36cc259b0c8452dfa5489a2b
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-29 09:35:12 -0300

    fix: routing entire app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,50 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '**', redirectTo: 'character-list', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
-  { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
-  { path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) },
-  { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) },
-  { path: 'not-found', loadChildren: () => import('./components/pages/not-found/not-found/not-found.module').then(m => m.NotFoundModule) }]
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'episodes',
+    loadChildren: () =>
+      import('./components/pages/episodes/episodes.module').then(
+        (m) => m.EpisodesModule
+      ),
+  },
+  {
+    path: 'character-list',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/character-list/character-list.module'
+      ).then((m) => m.CharacterListModule),
+  },
+  {
+    path: 'character-details/:id',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/character-details/character-details.module'
+      ).then((m) => m.CharacterDetailsModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./components/pages/about/about/about.module').then(
+        (m) => m.AboutModule
+      ),
+  },
+  {
+    path: 'not-found',
+    loadChildren: () =>
+      import('./components/pages/not-found/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
