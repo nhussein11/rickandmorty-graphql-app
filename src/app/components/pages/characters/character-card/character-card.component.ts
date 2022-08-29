753225bbfd0fc2671dc312bd9327ddb84864ba30
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-29 12:51:01 -0300

    add: change detection strategy

--- src/app/components/pages/characters/character-card/character-card.component.ts
+++ src/app/components/pages/characters/character-card/character-card.component.ts
@@ -1,16 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Character } from '@app/shared/models/data.interface';
 
 @Component({
   selector: 'app-character-card',
   templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.css']
+  styleUrls: ['./character-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CharacterCardComponent implements OnInit {
+export class CharacterCardComponent {
   @Input() character: Character = {} as Character;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
