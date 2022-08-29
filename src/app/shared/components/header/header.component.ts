6ce03d369cc30928900c566b53cf47c3779620a7
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-29 13:04:43 -0300

    add: routing to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ThemeService } from '@app/shared/services/theme.service';
 import { MenuItem } from 'primeng/api';
 
@@ -9,16 +10,25 @@ import { MenuItem } from 'primeng/api';
 })
 export class HeaderComponent implements OnInit {
   items: MenuItem[] = [];
-  constructor(private _themeService: ThemeService) {}
+  constructor(private _themeService: ThemeService, private _router: Router) {}
 
   ngOnInit(): void {
     this.items = [
       {
         label: 'Home',
         icon: 'pi pi-fw pi-home',
+        command: () => this._router.navigate(['/home']),
       },
-      { label: 'Characters', icon: 'pi pi-fw pi-users' },
-      { label: 'Episodes', icon: 'pi pi-fw pi-play' },
+      {
+        label: 'Characters',
+        icon: 'pi pi-fw pi-users',
+        command: () => this._router.navigate(['/character-list']),
+      },
+      {
+        label: 'Episodes',
+        icon: 'pi pi-fw pi-play',
+        command: () => this._router.navigate(['/episodes']),
+      },
       {
         label: 'Theme',
         icon: 'pi pi-fw pi-moon',
@@ -30,13 +40,9 @@ export class HeaderComponent implements OnInit {
   swtichTheme(): void {
     let indexLastItem: number = this.items.length - 1;
     this.items[indexLastItem].icon == 'pi pi-fw pi-moon'
-      ? (
-        this.items[indexLastItem].icon = 'pi pi-fw pi-sun',
-        this._themeService.swtichTheme('lightDeepPurple')
-        )
-      : (
-        this.items[indexLastItem].icon = 'pi pi-fw pi-moon',
-        this._themeService.swtichTheme('darkDeepPurple')
-        );
+      ? ((this.items[indexLastItem].icon = 'pi pi-fw pi-sun'),
+        this._themeService.swtichTheme('lightDeepPurple'))
+      : ((this.items[indexLastItem].icon = 'pi pi-fw pi-moon'),
+        this._themeService.swtichTheme('darkDeepPurple'));
   }
 }
