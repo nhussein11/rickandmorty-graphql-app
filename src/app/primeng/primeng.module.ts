d69d7a0ce11ba1c0b1434883b7c824894dd07f58
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-30 10:53:24 -0300

    add: character details styles

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DialogModule } from 'primeng/dialog';
+import {FieldsetModule} from 'primeng/fieldset';
+import {ImageModule} from 'primeng/image';
 import { ListboxModule } from 'primeng/listbox';
 import { TabMenuModule } from 'primeng/tabmenu';
 
@@ -10,6 +12,8 @@ import { TabMenuModule } from 'primeng/tabmenu';
     ButtonModule,
     CardModule,
     DialogModule,
+    FieldsetModule,
+    ImageModule,
     ListboxModule,
     TabMenuModule,
   ],
