0a49b81e3e32db1df93ff11160267a4ff430908e
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-30 09:27:01 -0300

    add: styles to characters list

--- src/app/components/pages/characters/character-details/character-details.component.ts
+++ src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from '@app/shared/models/data.interface';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
-  styleUrls: ['./character-details.component.css']
+  styleUrls: ['./character-details.component.css'],
 })
 export class CharacterDetailsComponent implements OnInit {
+  @Input() characterDetails: Character = {} as Character;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ListboxModule } from 'primeng/listbox';
 import { TabMenuModule } from 'primeng/tabmenu';
 
 @NgModule({
-  exports: [ButtonModule, CardModule, TabMenuModule],
+  exports: [ButtonModule, CardModule, ListboxModule, TabMenuModule],
 })
 export class PrimeNgModule {}
